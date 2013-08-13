bb08580bb2e8c5e093e3e8660dc828c875be7062
Nicholas Tarabelloni <nicholas.tarabelloni@gmail.com>
2013-08-13 19:10:59 +0200

    Started the process of templatization of BandDepth classes.
    Merged bandDepth.cpp file into the new bandDepth.hpp file.
    
    Following steps:
    
    1) Modifications of main (dropping for now the use of GetPot setup of J parameter )
    2) Specialization of computeBDs() method for bandDepth objects with J = 2
    3) Introduction of view by point in dataSet (view concept)
    4) Introduction of a Factory to restore the GetPot setup of J parameter
    5) Running tests.
    6) Performances?
    7) Merge into master

--- bandDepth.hpp
+++ bandDepth.hpp
@@ -10,6 +10,9 @@
 #include <boost/shared_ptr.hpp>
 #include <vector>
 #include <string>
+#include <cassert>
+#include <fstream>
+#include <mpi.h>
 
 /*!
     @file BandDepth.hpp
@@ -53,6 +56,7 @@ protected:
  * 
  */  
   
+template < UInt _J >
 class BandDepth : public BandDepthBase
 {
 public:
@@ -144,6 +148,9 @@ private:
   //! Each thread reads data from data files.
   void readData();
     
+  //! To compute binomial coefficients
+  binomial( const UInt & N , const UInt & K )
+  
   //@}
   
 
@@ -166,7 +173,247 @@ private:
     
 };
 
-UInt binomial( const UInt & N , const UInt & K );
+ /////////////////////////
+ // 	Band Depth
+ /////////////////////////
+ 
+ // Constructor from BandDepthData type object
+ template < UInt _J > 
+ BandDepth< _J >::
+ BandDepth( const bdData_Type & bdData )
+ :
+ M_bdDataPtr( new bdData_Type( bdData ) ),
+ M_mpiUtilPtr( new mpiUtility_Type() )
+ {   
+   if( bdData.readDataFromFile() ) this->readData();
+ }
+ 
+ template < UInt _J >
+ BandDepth< UInt _J >::
+ BandDepth( const bdDataPtr_Type & bdDataPtr )
+ :
+ M_bdDataPtr( bdDataPtr ),
+ M_mpiUtilPtr( new mpiUtility_Type() )
+ {      
+   if( this->M_bdDataPtr->readDataFromFile() ) this->readData();
+ }
+  
+ // Reset BandDepthData type object contained 
+ template < UInt _J >
+ inline
+ void 
+ BandDepth< _J >::
+ setBandDepthData( const bdData_Type & bdData )
+ {
+    this->M_bdDataPtr.reset( new bdData_Type( bdData ) );
+    
+    if ( bdData.readDataFromFile() ) this->readData();
+ }
+ 
+ // Reset dataSet pointer. This allow using the object without a file from which to read.
+ template < UInt _J >
+ void
+ BandDepth< UInt _J >::
+ setDataSet( const dataSetPtr_Type & dataPtr )
+ {
+   assert( 
+	  dataPtr->nbSamples() == this->M_bdDataPtr->nbPz() 
+	  && 
+	  dataPtr->nbPts() == this->M_bdDataPtr->nbPts() 
+	 );
+   
+   assert( not( this->M_bdDataPtr->readDataFromFile() ) );
+   
+   this->M_dataSetPtr = dataPtr;
+   
+   return;
+    
+ }
+ 
+ // Each proces reads data from data files.
+ template < UInt _J >
+ void 
+ BandDepth< _J >::
+ readData()
+ {
+   this->M_dataSetPtr.reset( new dataSet_Type( M_bdDataPtr->nbPz(), M_bdDataPtr->nbPts() ) );
+   
+   this->M_dataSetPtr->setOffset( M_bdDataPtr->leftOffset(), M_bdDataPtr->rightOffset() );
+   
+   this->M_dataSetPtr->readData( M_bdDataPtr->inputFilename() );
+   
+   return;
+ }
+ 
+ // Method for the computation of BDs
+ template < UInt _J >
+ void
+ BandDepth< UInt _J >::
+ computeBDs()
+ {
+   const UInt myRank = this->M_mpiUtilPtr->myRank();
+   const UInt nbThreads = this->M_mpiUtilPtr->nbThreads();
+   
+   const UInt MASTER = this->M_mpiUtilPtr->master();
+   
+   const UInt nbPz 	= this->M_bdDataPtr->nbPz();
+   const UInt nbPts	= this->M_bdDataPtr->nbPts();
+   const UInt J	   	= this->M_bdDataPtr->J();
+   const UInt verbosity = this->M_bdDataPtr->verbosity();
+   
+   typedef dataSet_Type::dataPtr_Type dataSetPtr_Type;
+   
+   const dataSetPtr_Type dataPtr( this->M_dataSetPtr->getData() );  
+            
+   const UInt slaveProcNbPz = static_cast< UInt >( nbPz / nbThreads );
+   const UInt masterProcNbPz = static_cast< UInt >( nbPz / nbThreads ) + static_cast< UInt >( nbPz % nbThreads );
+   
+   this->M_BDs.resize( nbPz );
+   
+   UInt nbMyPz;
+   
+   this->M_mpiUtilPtr->isMaster() ? nbMyPz = masterProcNbPz : nbMyPz = slaveProcNbPz;
+   
+   combinationsID_Type combinationsID( nbPz - 1, J, true );
+   
+   combinationsID.generateCombinationsID();
+   
+   for ( UInt iPz(0); iPz < nbMyPz; ++iPz )
+   {
+      if( verbosity > 2 ) printf( "Proc %d is at %d / %d patients\n", myRank, iPz+1, nbMyPz );
+	  
+      UInt globalPzID;
+     
+      this->M_mpiUtilPtr->isMaster() ? globalPzID = iPz : globalPzID = masterProcNbPz  + ( myRank - 1 ) * slaveProcNbPz + iPz;
+            
+      Real comprisedCounter(0);
+      
+      combinationsID.resetPointerToHeadCombination();      
+      
+//    while( not( combinationsID.hasGeneratedAll() ) )
+      while( not( combinationsID.hasTraversedAll() ) )
+      {
+	tuple_Type pzTupleIDs;
+
+	combinationsID.getNextCombinationID( pzTupleIDs );
+		
+	// IMPORTANT: mapping the IDs of the tuple to the IDs of the population
+	// The problem is that leaving one patient out will produce a tuple in
+	// the range [ 0, 1, ..., nbPz - 1], while the IDs out of the combinationsID
+	// are in the range [ 0, 1, ..., nbPz ] except for globalPzID.
+	for ( UInt iJ(0); iJ < J; ++iJ )
+	{	  
+	   if ( pzTupleIDs[ iJ ] >= globalPzID ) 
+	   {
+	       ++pzTupleIDs[ iJ ];
+	   }	  
+	}
+	
+	std::vector< Real > currentValues( J );
+	
+	Real envMax, envMin, pzVal;
+	
+	for ( UInt iPt(0); iPt < nbPts; ++iPt )
+	{
+	  for ( UInt iJ(0); iJ < J; ++iJ )
+	  {		
+	    currentValues[ iJ ] = (*dataPtr)( pzTupleIDs[ iJ ], iPt );
+	  } 
+	  
+	  envMax =  *( std::max_element( currentValues.begin(), currentValues.end() ) );
+	  envMin =  *( std::min_element( currentValues.begin(), currentValues.end() ) );
+	  pzVal = (*dataPtr)( globalPzID, iPt );
+	  
+	  if ( pzVal <= envMax && pzVal >= envMin ) ++comprisedCounter;  
+	  
+	}
+      }
+
+	this-> M_BDs[ iPz ] = comprisedCounter / static_cast< Real > ( ( nbPts - 1 ) * this->binomial( nbPz - 1, J ) );
+  }	    
+ 
+  // COMMUNICATING BAND DEPTHS
+    
+  for ( UInt iThread = 1; iThread < nbThreads; ++iThread )
+  {
+    if ( this->M_mpiUtilPtr->isMaster() )
+    {
+      MPI_Status status;
+
+      MPI_Recv( & this->M_BDs[0] + masterProcNbPz + ( iThread - 1 ) * slaveProcNbPz, slaveProcNbPz, MPI_DOUBLE_PRECISION, iThread, iThread, MPI_COMM_WORLD, & status );
+    }
+    else if ( myRank == iThread )
+    {
+      MPI_Send( & this->M_BDs[0], slaveProcNbPz, MPI_DOUBLE_PRECISION, MASTER, myRank, MPI_COMM_WORLD );
+    }
+  }
+  
+  if ( verbosity > 2 && this->M_mpiUtilPtr->isMaster() ) 
+
+    printf( " All depths have been gathered\n" );
+  
+   return;
+  
+} 
+
+// Method to write the computed BDs
+template < UInt _J >
+void
+BandDepth< UInt _J >::
+writeBDs() const
+{    
+    if ( this->M_mpiUtilPtr->isMaster() )
+    {
+      std::ofstream output( this->M_bdDataPtr->outputFilename().data(), std::ios_base::out );
+      
+      for( UInt iPz(0); iPz < this->M_bdDataPtr->nbPz(); ++iPz )
+      {
+	  output << iPz << " " << this->M_BDs[ iPz ] << std::endl;
+      }
+      
+      output.close();
+    }
+  
+    return;
+}
+
+
+// Method to get the computed BDs
+template < UInt _J >
+inline 
+const
+std::vector< Real > &
+BandDepth< UInt _J >::
+getBDs() const
+{
+    return this->M_BDs;
+}
+
+
+// Method for the computation of binomial coefficients
+template < UInt _J >
+UInt
+BandDepth< UInt _J >::
+binomial( const UInt & N , const UInt & K )
+{    
+    UInt num( 1 );
+    UInt denom( 1 );
+    
+    for ( UInt iK(0); iK < K; ++iK )
+    {
+	num *= N - iK;
+	denom *= iK + 1;
+    }
+  
+   return static_cast< UInt >( num/denom );
+}
+
+// NOW YOU HAVE TO SPECIALIZE THE COMPUTE METHOD FOR THE CLASS WITH J = 2.
+
+/* 
+ * 1) Use a view of data set by points
+ * 2) Implement Genton idea. 
+ */
 
 }
 
